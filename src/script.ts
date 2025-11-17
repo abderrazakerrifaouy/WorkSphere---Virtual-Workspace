ef8fae2432d9a2276d7a09adb824f664093d6803
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2025-11-17 16:47:30 +0100

    create interfase Experence and personProfile

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,22 @@
-console.log("test")
+
+interface Experience {
+  company: string;
+  position: string;
+  startDate: string; 
+  endDate?: string;  
+  description?: string;
+}
+
+interface PersonProfile {
+  id: Number;
+  nom: string;
+  role: string;
+  photoUrl?: string;
+  email?: string;
+  telephone?: string;
+  experiences: Experience[];
+}
+
 
 
 
