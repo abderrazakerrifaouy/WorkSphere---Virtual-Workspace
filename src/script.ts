863769fcb6b56a333ab2975752c827169b2c4a13
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2025-11-20 23:26:22 +0100

    linke la doome to htmml

---
+++ src/script.ts
@@ -0,0 +1,43 @@
+import {aficherForemAjouterPerson , closeForemAjouterPerson , addExperions , getProfileData , afficherLesPerson , ajouterToZone} from './doom.js'
+import { addToZone } from './model.js';
+
+let addPerson  = document.querySelector("#addProfile") as HTMLButtonElement
+let closseForet = document.querySelector("#closeIcone") as HTMLImageElement
+let addExperienceBtn = document.querySelector("#addExperienceBtn") as HTMLButtonElement;
+let profileForm = document.querySelector("#profileForm") as HTMLFormElement
+let AddtoZone = document.querySelectorAll("#AjouterToZone") 
+
+
+addPerson.addEventListener("click" , ()=>aficherForemAjouterPerson())
+closseForet.addEventListener("click" , ()=>closeForemAjouterPerson())
+addExperienceBtn.addEventListener("click" ,()=>addExperions() )
+
+
+
+
+profileForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const isValid = await getProfileData();
+
+    if (isValid) {
+        closeForemAjouterPerson();
+        profileForm.reset();
+    }
+});
+
+
+
+
+
+afficherLesPerson()
+
+Array.from(AddtoZone).forEach((elemet)=>{
+  elemet.addEventListener("click", ()=>ajouterToZone(elemet))
+})
+
+
+document.querySelector("#deleteFiltrage")?.addEventListener("click" , ()=>{
+  afficherLesPerson()
+})
+
--- src/scripts/src/script.js
+++ src/scripts/src/script.js
@@ -1,1 +1,34 @@
-"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a;
+import { aficherForemAjouterPerson, closeForemAjouterPerson, addExperions, getProfileData, afficherLesPerson, ajouterToZone } from './doom.js';
+let addPerson = document.querySelector("#addProfile");
+let closseForet = document.querySelector("#closeIcone");
+let addExperienceBtn = document.querySelector("#addExperienceBtn");
+let profileForm = document.querySelector("#profileForm");
+let AddtoZone = document.querySelectorAll("#AjouterToZone");
+addPerson.addEventListener("click", () => aficherForemAjouterPerson());
+closseForet.addEventListener("click", () => closeForemAjouterPerson());
+addExperienceBtn.addEventListener("click", () => addExperions());
+profileForm.addEventListener("submit", (e) => __awaiter(void 0, void 0, void 0, function* () {
+    e.preventDefault();
+    const isValid = yield getProfileData();
+    if (isValid) {
+        closeForemAjouterPerson();
+        profileForm.reset();
+    }
+}));
+afficherLesPerson();
+Array.from(AddtoZone).forEach((elemet) => {
+    elemet.addEventListener("click", () => ajouterToZone(elemet));
+});
+(_a = document.querySelector("#deleteFiltrage")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    afficherLesPerson();
+});
